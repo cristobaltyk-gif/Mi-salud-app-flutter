e9f3294e82c16654c56043e6a1c22c86fd32c2d6
cristobaltyk-gif <cristobal.tyk@gmail.com>
2026-07-03 20:20:08 -0400

    Create app_colors.dart

---
+++ lib/theme/app_colors.dart
@@ -0,0 +1,11 @@
+/// lib/theme/app_colors.dart
+///
+/// Colores de marca HypokratIA, usados en toda la app.
+library;
+
+import 'package:flutter/material.dart';
+
+const Color kHypoBlue = Color(0xFF1A3B8C);
+const Color kHypoBlueDark = Color(0xFF0F235A);
+const Color kHypoTeal = Color(0xFF0F9B8E);
+const Color kHypoTealDark = Color(0xFF0B2E45);
